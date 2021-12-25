4e32bd0a8bca2e8d8e3b4025142c719c4db90216
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-12-25 20:44:27 +0100

    feat: improve folder structure and add a new page Home

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Home } from "./src/pages/Home";
 
 export default function App() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Hello World</Text>
-    </View>
-  );
+  return <Home />;
 }
